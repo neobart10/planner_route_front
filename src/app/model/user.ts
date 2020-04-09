1ed42c4ae63d2ced7f6b566c77b8d29bd5992695
Dixon Medina <neobart10@gmail.com>
2020-04-08 21:17:03 -0500

    neobart10 - 2020/04/08

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -1,12 +1,11 @@
 export class User {
-    constructor(public id: number, public username: string, public pass: string, 
-        public typeVehicle: number, public gallonsVehicle: number, public gallonsKm: number) {
+    constructor(public id: number, public username: string, public pass: string,
+                public typeVehicle: number, public gallonsVehicle: number, public gallonsKm: number) {
     }
-  
+
     static fromJson(json): User {
       return new User(json.id, json.username, json.pass, json.typeVehicle,
          json.gallonsVehicle, json.gallonsKm
         );
     }
-  }
-  
+}
--- src/app/service/city.service.ts
+++ src/app/service/city.service.ts
@@ -1,9 +1,10 @@
-import {Observable} from 'rxjs/Observable';
 import {Injectable} from '@angular/core';
 import {HttpUtil} from '../util/http.util';
 import {map} from 'rxjs/operators';
 import {environment} from '../../environments/environment';
 import {City} from '../model/city';
+import {Observable} from 'rxjs';
+import {HttpHandler, HttpHeaders, HttpRequest} from '@angular/common/http';
 
 @Injectable()
 export class CityService {
@@ -11,12 +12,38 @@ export class CityService {
   constructor(private http: HttpUtil) {
   }
 
-  getCities(): Observable<Array<City>> {
+  getAll(): Observable<Array<City>> {
     return this.http.get(environment.url + '/city').pipe(map(
       (data: any) =>
         data.map(
           json => City.fromJson(json)
         )
+    ));
+  }
+
+
+  get(id): Observable<City> {
+    return this.http.get(environment.url + '/city/' + id).pipe(map(
+      (data: any) =>
+        json => City.fromJson(json)
+    ));
+  }
+
+  save(city): Observable<City> {
+    return this.http.post(environment.url + '/city', JSON.stringify(city),
+      new HttpHeaders({'Content-Type' : 'application/json; charset=UTF-8;'})).pipe(map(
+      (data: any) => {
+        if (data) { return City.fromJson(data); } else { return data; }
+      }
+    ));
+  }
+
+  update(city, id): Observable<City> {
+    return this.http.put(environment.url + '/city/id', JSON.stringify(city),
+      new HttpHeaders({'Content-Type' : 'application/json; charset=UTF-8;'})).pipe(map(
+      (data: any) => {
+        if (data) { return City.fromJson(data); } else { return data; }
+      }
     ));
   }
 }
