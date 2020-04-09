f6774b5b58d48a6337b9353aa7eed6f355644fee
Dixon Medina <neobart10@gmail.com>
2020-04-09 10:21:18 -0500

    neobart10 - 2020/04/09 - delete example city

--- src/app/model/city.ts
+++
@@ -1,8 +0,0 @@
-export class City {
-  constructor(public codigo: number, public descripcion: string) {
-  }
-
-  static fromJson(json): City {
-    return new City(json.codigo, json.descripcion);
-  }
-}
