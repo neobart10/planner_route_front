0dd6484fe1be45d3d3e19fc7bd15f5a57f1b6a0f
Ikgomezm <ikgomezm@gmal.com>
2020-04-08 23:34:43 -0500

    Ingrid Gomez

---
+++ src/app/service/route.services.ts
@@ -0,0 +1,63 @@
+import {Injectable} from '@angular/core';
+import {HttpUtil} from '../util/http.util';
+import {map} from 'rxjs/operators';
+import {environment} from '../../environments/environment';
+import {Route} from '../model/route';
+import {Observable} from 'rxjs';
+import {HttpHandler, HttpHeaders, HttpRequest} from '@angular/common/http';
+
+@Injectable()
+export class RouteService {
+
+  constructor(private http: HttpUtil) {
+  }
+
+  getAll(): Observable<Array<Route>> {
+    return this.http.get(environment.url + '/route').pipe(map(
+      (data: any) =>
+        data.map(
+          json => Route.fromJson(json)
+        )
+    ));
+  }
+
+
+  get(id): Observable<Route> {
+    return this.http.get(environment.url + '/route' + id).pipe(map(
+      (data: any) =>
+        json => Route.fromJson(json)
+    ));
+  }
+  getById(idUser): Observable<Route> {
+    return this.http.get(environment.url + '/route/idUser' + idUser).pipe(map(
+      (data: any) =>
+        json => Route.fromJson(json)
+    ));
+  }
+  save(route): Observable<Route> {
+    return this.http.post(environment.url + '/route/', JSON.stringify(route),
+      new HttpHeaders({'Content-Type' : 'application/json; charset=UTF-8;'})).pipe(map(
+      (data: any) => {
+        if (data) { return Route.fromJson(data); } else { return data; }
+      }
+    ));
+  }
+
+  update(route, id): Observable<Route> {
+    return this.http.put(environment.url + '/plan/id', JSON.stringify(route),
+      new HttpHeaders({'Content-Type' : 'application/json; charset=UTF-8;'})).pipe(map(
+      (data: any) => {
+        if (data) { return Route.fromJson(data); } else { return data; }
+      }
+    ));
+  }
+
+  delete(route, id): Observable<Route> {
+    return this.http.put(environment.url + '/plan/id', JSON.stringify(route),
+      new HttpHeaders({'Content-Type' : 'application/json; charset=UTF-8;'})).pipe(map(
+      (data: any) => {
+        if (data) { return Route.fromJson(data); } else { return data; }
+      }
+    ));
+  }
+}
---
+++ src/app/service/user.service.ts
@@ -0,0 +1,49 @@
+import {Injectable} from '@angular/core';
+import {HttpUtil} from '../util/http.util';
+import {map} from 'rxjs/operators';
+import {environment} from '../../environments/environment';
+import {Observable} from 'rxjs';
+import {HttpHandler, HttpHeaders, HttpRequest} from '@angular/common/http';
+import { User } from '../model/user';
+
+@Injectable()
+export class UserService {
+
+  constructor(private http: HttpUtil) {
+  }
+
+  getAll(): Observable<User> {
+    return this.http.get(environment.url + '/user').pipe(map(
+      (data: any) =>
+        data.map(
+          json => User.fromJson(json)
+        )
+    ));
+  }
+
+
+  get(id): Observable<User> {
+    return this.http.get(environment.url + '/user/' + id).pipe(map(
+      (data: any) =>
+        json => User.fromJson(json)
+    ));
+  }
+
+  save(user): Observable<User> {
+    return this.http.post(environment.url + '/user', JSON.stringify(user),
+      new HttpHeaders({'Content-Type' : 'application/json; charset=UTF-8;'})).pipe(map(
+      (data: any) => {
+        if (data) { return User.fromJson(data); } else { return data; }
+      }
+    ));
+  }
+
+  update(user, id): Observable<User> {
+    return this.http.put(environment.url + '/user/id', JSON.stringify(user),
+      new HttpHeaders({'Content-Type' : 'application/json; charset=UTF-8;'})).pipe(map(
+      (data: any) => {
+        if (data) { return User.fromJson(data); } else { return data; }
+      }
+    ));
+  }
+}
