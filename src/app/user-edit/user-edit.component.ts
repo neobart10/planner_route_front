66f89c408fd2b4ee6f8c7a8598fb428b20dc6b21
Dixon Medina <neobart10@gmail.com>
2020-04-12 21:32:40 -0500

    neobart10 - 2020/04/12 - center udate user

--- src/app/user-edit/user-edit.component.ts
+++ src/app/user-edit/user-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {User} from '../model/user';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import {UserService} from '../service/user.service';
@@ -17,15 +17,13 @@ export class UserEditComponent implements OnInit {
   public usernameEmpty = false;
   public passEmpty = false;
   public errorLogin = false;
-  public userId : number;
-
+  public userId: number;
+  private keyUser = '&I%U%$234';
 
   public typeVehicle = [
     {value: 1, viewValue: 'Car'},
     {value: 2, viewValue: 'Bike'}
   ];
-
-  keyUser = '&I%U%$234';
 
   public gallons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
@@ -37,31 +35,27 @@ export class UserEditComponent implements OnInit {
 
   ngOnInit(): void {
 
-<<<<<<< HEAD
+    this.userId = Number(this._cookieService.get(this.keyUser));
 
+    this.userService.get(this.userId).subscribe(
+      user => {
+        this.user = user;
+      }
+    );
 
-=======
-     this.userId = Number (this._cookieService.get(this.keyUser));
-
-      this.userService.get(this.userId).subscribe(  
-        user => {          
-          this.user = user;          
-        }
-      );
->>>>>>> 1656b73d9b35ecdeb0e076413c70903824aa145a
   }
 
   onSave() {
     if (this.isValid()) {
       this.loading = true;
-      this.userService.update(this.user,this.user.id).subscribe(
+      this.userService.update(this.user, this.user.id).subscribe(
         user => {
           this.loading = false;
           if (user === null) {
             this.errorLogin = true;
             this.openSnackBar('user not exist.', 'Retry');
           } else {
-            this.openSnackBar('User Updated', 'Welcome');            
+            this.openSnackBar('User Updated', 'Welcome');
           }
           this.user.username = '';
           this.user.pass = '';
@@ -71,19 +65,19 @@ export class UserEditComponent implements OnInit {
     }
   }
 
-  isValid(){
+  isValid() {
     this.passEmpty = false;
     this.usernameEmpty = false;
     this.errorLogin = false;
 
     let result = true;
 
-    if (this.user.username === null || this.user.username.length === 0 ){
+    if (this.user.username === null || this.user.username.length === 0) {
       this.usernameEmpty = true;
       result = false;
     }
 
-    if (this.user.pass === null || this.user.pass.length === 0){
+    if (this.user.pass === null || this.user.pass.length === 0) {
       this.passEmpty = true;
       result = false;
     }
@@ -98,8 +92,7 @@ export class UserEditComponent implements OnInit {
   }
 
 
-
-  onLogOut(){
+  onLogOut() {
     this._cookieService.remove(this.keyUser);
     this.router.navigate(['login']);
   }
