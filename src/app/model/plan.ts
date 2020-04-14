34ba77cb495115c535beab6677a61d55c1a9ca03
Jonatan Jimenez <jajimenezpa@poligran.edu.co>
2020-04-14 08:04:56 -0500

    jonatanj1293 14/04/2020

--- src/app/model/plan.ts
+++ src/app/model/plan.ts
@@ -1,13 +1,13 @@
 import { Route } from './route';
 
 export class Plan {
-    constructor(public id: number, public route: Route, public startLat: number,
-                public startLng: number, public targetLat: number, public targetLng: number,
+    constructor(public id: number, public route: Route, public description: string,
+                public lat: number, public lng: number, public km: number, 
                 public stop: number, public state: number) {
     }
 
     static fromJson(json): Plan {
         return new Plan(json.id, json.route ? Route.fromJson(json.route) : null,
-        json.startLat, json.startLng, json.targetLat, json.targetLng, json.stop, json.state);
+        json.description, json.lat, json.lng, json.km, json.stop, json.state);
     }
 }
